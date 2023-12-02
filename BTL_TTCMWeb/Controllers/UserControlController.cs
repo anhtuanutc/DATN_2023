4b6888bba474f0022a4fe47de699f4456b777914
tuanna2001 <71838457+anhtuandl@users.noreply.github.com>
2023-12-02 09:26:27 +0700

    Đẩy đồ án lên git
    Đẩy đồ án lên git

---
+++ BTL_TTCMWeb/Controllers/UserControlController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTL_TTCMWeb.Controllers
+{
+    public class UserControlController : Controller
+    {
+        // GET: UserControl
+        public ActionResult UserControl()
+        {
+            return View();
+        }
+    }
+}
